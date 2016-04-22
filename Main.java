905cefee27483e25d50ac691535b931db1fa9f71
Pierre-F <peuf72@gmail.com>
2016-04-22 22:36:22 +0200

    ajout de boucle (test)

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world !");
+		for(int i =0;i<10;i++){
+			System.out.println("Hello world !");
+		}
 	}
 
 }
