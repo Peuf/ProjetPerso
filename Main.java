a621a5e9df5e915357687ae7f02548ddbb8385e6
Pierre-F <peuf72@gmail.com>
2016-04-25 16:21:27 +0200

    modif Syso

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i =0;i<10;i++){
-			System.out.println("Hello world !");
+			System.out.println(i + "Hello world !");
 		}
 	}
 
